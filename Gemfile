ccb72d05d171656416a44c30c8493022c3302a01
Karume629 <karumekate8@gmail.com>
2023-01-23 06:17:00 +0300

    generated user resource

--- Gemfile
+++ Gemfile
@@ -20,7 +20,11 @@ gem "rack-cors"
 
 gem "bcrypt"
 
-gem "active_model_serializer"
+gem 'active_model_serializers', '~> 0.10.2'
+
+gem "kaminari"
+
+gem "faker"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
---
+++ db/migrate/20230123031625_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :phone_number
+      t.string :email
+      t.string :first_name
+      t.string :last_name
+      t.string :password_digest
+      t.string :avatar
+
+      t.timestamps
+    end
+  end
+end
