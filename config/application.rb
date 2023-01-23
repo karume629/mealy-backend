c75d971613a8ea89996a81060cd94dfb4c70ac77
Karume629 <karumekate8@gmail.com>
2023-01-23 07:28:05 +0300

    add use of session

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,12 @@ Bundler.require(*Rails.groups)
 module KatesInn
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+
+    config.action_dispatch.cookies_same_site_protection = :strict
+
     config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
