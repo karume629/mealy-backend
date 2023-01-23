e11b39651da4dc0e2fcc590b6a29575df44aea1f
Karume629 <karumekate8@gmail.com>
2023-01-23 07:23:51 +0300

    user authentication and pagination implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,58 @@
 class UsersController < ApplicationController
+    def index
+        render json: User.page(page).per(per_page), status: :ok
+    end
+
+    def create
+        user = User.create!(user_params)
+        if user.valid?
+            render json: user, status: :created
+        else
+            render json: user.errors, status: :unprocessable_entity        
+        end
+    end
+
+    def show
+       user = User.find(session[:id]) 
+       if user 
+           render json: user, status: :ok
+        else
+            render json: user.errors, status: :unprocessable_entity
+       end
+    end
+
+    def update
+        user = User.find(session[:id])
+        if user
+            user.update!(user_params)
+            render json: user, status: :accepted
+        else
+            render json: user.errors, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        user = User.find(session[:id])
+        if user
+            user.destroy
+            render json: {success: "Deleted"}, status: :no_content
+        else
+            render json: user.errors, status: :unprocessable_entity
+        end
+    end
+
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :email, :password_confirmation, :first_name, :last_name, :avatar, :phone_number)
+    end
+
+    def per_page
+        @per_page ||= params[:per_page] || 10
+    end
+
+    def page
+        @page ||= params[:page] || 1
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Creating database please wait..."
+
+13.times do | i |
+    User.create(username: "user_#{i +1}", phone_number:  Faker::Number.number(digits: 10), email: Faker::Internet.email, password: "Abzedar#{i+1}", first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, avatar: Faker::Avatar.image())    
+end
+
+puts "Done!"
